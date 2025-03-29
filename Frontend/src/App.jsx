bb7de16eee7cc7df93aa9805673f790f54059fad
RaviKant <rk3727000@gmail.com>
2025-03-29 23:30:07 +0530

    migrating from v3 to v4 of tailwind css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-    <h1>Welcome DTU Developers</h1>
+    <h1 className='text-3xl bg-red-400'>Welcome DTU Developers</h1>
      <p>
       Let&apos;s build the Official GDSC Website!<br></br>
      </p>
