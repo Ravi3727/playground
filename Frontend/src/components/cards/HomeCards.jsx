2fe1b132fa00fdf4cceb6c15e4b266c64d5ce764
sahilchauhan11 <sahilsingh982167@gmail.com>
2025-03-30 10:23:20 +0530

    Added Bottom Section of Home page

---
+++ Frontend/src/components/cards/HomeCards.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HomeCards = ({ color,obj }) => {
+    return (
+        <div style={{ borderTopColor: color }} className={`w-[407px] h-[217px] border-t-3 $ border-x-2 border-x-gray-200 py-3 px-3 rounded-2xl 0 flex flex-col justify-between gap-4`}>
+            <div>
+                <h1 className='font-bold text-left'>{obj.name}</h1>
+                <p className='text-[#696969] text-sm text-left'>{obj.description}
+                </p>
+            </div>
+            <a className={` text-start`} style={{ color: color }} href="">learn more ></a>
+
+        </div>
+    )
+}
+
+export default HomeCards
