f27690a26290d21e41d33b31d084d037d6498565
RaviKant <rk3727000@gmail.com>
2025-03-30 15:18:39 +0530

    correct vishesh code config

--- Frontend/postcss.config.js
+++ Frontend/postcss.config.js
@@ -1,7 +1,6 @@
-// filepath: c:\Users\ayush\Downloads\gdsc\playground\Frontend\postcss.config.js
 export default {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 }
