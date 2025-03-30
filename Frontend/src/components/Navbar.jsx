07c4b0f62a167e3c876e3539e90cbf1df10d2501
RaviKant <rk3727000@gmail.com>
2025-03-30 14:59:40 +0530

    resolved conflict

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import "../styles/Navbar.css";
-import gdgLogo from "../assets/ProjectPageIcons/gdg.svg";
+import React from 'react';
 
-const Navbar = () => {
+function NavBar() {
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <img
-          src={gdgLogo}
-          alt="Logo"
-          className="navbar-logo-img"
-        />
-        
-      </div>
-      <ul className="navbar-links">
-        <li>Resources & Blogs</li>
-        <li className="active">Projects</li>
-        <li>Events</li>
-        <li>Alumni</li>
+    <nav className='flex items-center justify-between bg-[#e5e4e4] rounded-4xl w-[80%] mx-auto mt-6 py-3 px-6 shadow-lg'>
+      {/* Left: Logo */}
+      <a href="#" className="logo font-bold text-lg">
+        <img className='w-[50px] h-[30px]' src="logo.png" alt="logo" />
+      </a>
+
+      {/* Center: Navigation Links */}
+      <ul className="flex items-center ml-22 gap-10">
+        <li><a href="#" className="hover:text-gray-600">Resources & Blogs</a></li>
+        <li><a href="#" className="hover:text-gray-600">Projects</a></li>
+        <li><a href="#" className="hover:text-gray-600">Events</a></li>
+        <li><a href="#" className="hover:text-gray-600">Alumni</a></li>
       </ul>
-      <button className="navbar-login">Log in</button>
+
+      {/* Right: Login Button */}
+      <button className='px-4 py-2 bg-black text-white text-[15px] rounded-3xl w-[140px] transform active:scale-90 cursor-pointer hover:bg-slate-900 transition'>
+        Log In/ Sign UP
+      </button>
     </nav>
   );
-};
+}
 
-export default Navbar;
+export default NavBar;
