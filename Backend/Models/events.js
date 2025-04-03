2f0e21a818d63b2cef3f08a6346da8f5856d8168
Sonal-ai <sonalkhanak@gmail.com>
2025-04-03 15:52:11 +0530

    added event model

---
+++ Backend/Models/events.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+
+const EventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    on_date: {
+      type: Date,
+      required: true,
+    },
+    venue: {
+      mode: {
+        type: String,
+        enum: ["online", "offline"],
+        required: true,
+      },
+      place: {
+        type: String,
+        required: true,
+      },
+    },
+    registered_user_id: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        unique: true, // Ensures a user can't register twice
+      },
+    ],
+    department: [
+      {
+        type: String,
+        enum: [
+          "web dev",
+          "cybersecurity",
+          "AI/ML",
+          "Open Source",
+          "App dev",
+          "UI/UX",
+        ],
+        required: true,
+      },
+    ],
+    winners: [
+      {
+        user_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        position: {
+          type: String,
+          enum: ["1st", "2nd", "3rd", "Honorable Mention"], // Add more as needed
+        },
+      },
+    ],
+    type_of_event: {
+      type: String,
+      enum: ["Hackathon", "Speaker Session", "Workshop", "Meetup"],
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Event = mongoose.model("Event", EventSchema);
+
+module.exports = Event;
