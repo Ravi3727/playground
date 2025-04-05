583602228ae16eee3c373cdcc9ea4ec0e1f54ab7
AYUSH-0305 <9811658802ayushmansingh@gmail.com>
2025-04-05 22:51:21 +0530

    Created Project page api  with ClerkId-based authentication

--- Backend/Routes/index.js
+++ Backend/Routes/index.js
@@ -3,9 +3,10 @@ const router = Router();
 import departmentRouter from "./department.route.js";
 import teamRouter from "./team.route.js";
 import signupRouter from "./signup.route.js";
+import { verifyClerkAuth } from "../Middlewares/clerkIdAuth.js";
 
-router.use("/department", departmentRouter);
-router.use("/team", teamRouter);
+router.use("/department", verifyClerkAuth, departmentRouter);
+router.use("/team", verifyClerkAuth, teamRouter);
 router.use("/signup", signupRouter);
 
 export default router;
