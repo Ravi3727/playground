a042379e6299aa3c242168471461f456b73c3ff9
Sonal-ai <sonalkhanak@gmail.com>
2025-04-05 21:04:16 +0530

    merging

--- Backend/index.js
+++ Backend/index.js
@@ -1,31 +1,24 @@
-require("dotenv").config();
-const express = require("express");
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import { connectDB } from './DBConfig/demo.js';
+import authRoutes from './Routes/demo.js'
+
+dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to GDSC Web Server!");
 });
 
+app.use('/', authRoutes);
+
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-const dbConnect = require("./DBConfig/DBConfig");
-
-dbConnect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("connected to mongoDB successfully");
-  })
-  .catch((err) => {
-    console.log("connection failed : ", err);
-  });
- 
-  const EventRouter = require('./Routes/events');
-  const userRoutes = require("./Routes/user");
-
-  app.use('/api/v1/events',EventRouter);
-  app.use("/api/v1/users", userRoutes);
