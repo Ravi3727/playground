85a3d12c5300ae6dd4665ff12b84abdf15cba15c
RaviKant <rk3727000@gmail.com>
2025-04-09 23:28:03 +0530

    Revert PR 23 and resolved conflicts

--- Backend/Routes/department.route.js
+++ Backend/Routes/department.route.js
@@ -2,8 +2,6 @@ import validateSchema from "../Middlewares/validate.middleware.js";
 import { Router } from "express";
 import { departmentSchema, updateDepartmentSchema } from "../Validations/department.validation.js";
 import { postDepartment, getDepartment, getDepartmentById, updateDepartment } from "../Controllers/department.controller.js";
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 const router = Router();
 
@@ -16,24 +14,19 @@ router.route("/:id").get(getDepartmentById);
 router.route("/:id").put(validateSchema(updateDepartmentSchema), updateDepartment);
 // Middleware required for authentication, should be put before validdate Schema middleware
 // Middleware should return status 403 if not authenticated.
-=======
+
 import { verifyClerkAuth } from "../Middlewares/clerkIdAuth.js";
 
 const router = Router();
 
-=======
 import { verifyClerkAuth } from "../Middlewares/clerkIdAuth.js";
 
 const router = Router();
 
->>>>>>> parent of da71cb8 (Merge pull request #23 from AYUSH-0305/ayushman)
 router.route("/").post(verifyClerkAuth, validateSchema(departmentSchema), postDepartment);
 router.route("/").get(verifyClerkAuth, getDepartment);
 router.route("/:id").get(verifyClerkAuth, getDepartmentById);
 router.route("/:id").put(verifyClerkAuth, validateSchema(updateDepartmentSchema), updateDepartment);
-<<<<<<< HEAD
->>>>>>> parent of da71cb8 (Merge pull request #23 from AYUSH-0305/ayushman)
-=======
->>>>>>> parent of da71cb8 (Merge pull request #23 from AYUSH-0305/ayushman)
+
 
 export default router;
