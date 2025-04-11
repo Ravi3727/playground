0046bcb09408232b286d7ae10fdbc888c5abbcf7
sahilchauhan11 <sahilsingh982167@gmail.com>
2025-04-11 20:34:05 +0530

    added blogroutes

--- Backend/Routes/index.js
+++ Backend/Routes/index.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import departmentRouter from "./department.route.js";
 import teamRouter from "./team.route.js";
-
+import blogRouter from "./blog.route.js"
 import resourceRouter from "./resource.route.js";
 import userRouter from "./user.route.js";
 import eventRoutes from "./events.route.js";
@@ -9,7 +9,7 @@ import eventRoutes from "./events.route.js";
 const router = Router();
 router.use("/department", departmentRouter);
 router.use("/team", teamRouter);
-
+router.use("/blogs",blogRouter)
 router.use("/user", userRouter);
 router.use("/resources", resourceRouter);
 router.use("/events", eventRoutes);
